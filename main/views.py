1992d959253189c57acdff41adcf348f231b1fa7
poffir <poffir4ez@gmail.com>
2025-01-24 19:22:13 +0200

    commit 2

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.filter(available = True)[:16]
+    return render(request, "main/index.html",
+                  {"products": products})
+
+def product_detail(request, slug):
+    pass
